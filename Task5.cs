574703d1d28fd37576a56e16a8ebbba631ab562a
Marian Vasylyk <marianasylykua@gmail.com>
2019-08-19 13:57:52 +0300

    1.0

--- Task5.cs
+++ Task5.cs
@@ -18,7 +18,7 @@ namespace Task5
             Console.WriteLine("2)Division");
 
             input = Console.ReadKey();
-            switch(input.Key)
+            switch (input.Key)
             {
                 case ConsoleKey.D1:
                     Menu(true);
@@ -51,19 +51,28 @@ namespace Task5
             Console.WriteLine("Enter the imaginary part of your number:");
             SecondImagin = Checker();
 
+            var value1 = new ComplexNumber(FirstReal, FirstImagin);
+            var value2 = new ComplexNumber(SecondReal, SecondImagin);
             if (temp)
             {
-
+                result = ComplexToString(value1 * value2);
             }
             else if (!temp)
             {
-
+                result = ComplexToString(value1 / value2);
             }
 
             Console.WriteLine("The result of multiplicating your numbers is - " + result + 'i');
             Console.ReadKey();
 
             PracticalTaks.Program.Main();
+        }
+
+        static private string ComplexToString(Complex a)
+        {
+            string text = a.ToString();
+            text = text.Trim(new char[] { '(', ')' });
+            return text;
         }
         
         static private double Checker()
@@ -93,23 +102,30 @@ namespace Task5
 
     class ComplexNumber
     {
-        public static double real;
-        public static double imagin;
+        private readonly double Real;
+        private readonly double Imagin;
+        private readonly Complex Value;
+
+        public ComplexNumber(double real, double imagin)
+        {
+            Real = real;
+            Imagin = imagin;
+            Complex value = new Complex(Real, Imagin);
+        }
+
+        public ComplexNumber(Complex value)
+        {
+            Value = value;
+        }
 
         #region Multiplication
-        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
-        {
-            return a;
-        }
+        public static Complex operator *(ComplexNumber a, ComplexNumber b)
+            =>Complex.Multiply(new Complex(a.Real,a.Imagin), new Complex(b.Real, b.Imagin));
         #endregion
 
         #region Division
-        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
-        {
-            ComplexNumber num = new ComplexNumber();
-            Complex num1 = new Complex(real,imagin);
-            return num;
-        }
+        public static Complex operator /(ComplexNumber a, ComplexNumber b)
+            => Complex.Divide(new Complex(a.Real, a.Imagin), new Complex(b.Real, b.Imagin));
         #endregion
     }
 }
